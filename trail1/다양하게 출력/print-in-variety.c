4b5730d958efd8e5497532829701882bc81af511
Doyun Lee <134493573+CodingKingDoyun@users.noreply.github.com>
2026-09-16 22:34:31 +0900

    [Passed] 다양하게 출력 (1ms, 0MB)

---
+++ trail1/다양하게 출력/print-in-variety.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    printf("%s\n%d\n%s\n%.10f","Total days in Year",365,"Circumference rate",3.1415926535);
+    return 0;
+}
